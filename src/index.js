c208d9cbe4e489e6665336e0614b5d7c45165930
robber-AS <robberbonner@gmail.com>
2026-08-25 11:37:12 -0300

    robson navegação

--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ import './index.css';
 import App from './App';
 import Eventos from './pages/eventos';
 import './pages/eventos/index.css';
+import Contato from './pages/contato';
+import './pages/contato/index.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Eventos />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+        <Route path='/contato' element={<Contato/>}/>
+        <Route path='/eventos' element={<Eventos/>}/>
+      </Routes>
+    
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ src/pages/contato/index.jsx
@@ -0,0 +1,15 @@
+import './index.css';
+
+export default function  Contato(){
+    return(
+
+        <div className='pagina-contato'>
+            <h1>Contato</h1>
+            <p>Entre em  contato</p>
+
+
+
+
+        </div>
+    );
+}
